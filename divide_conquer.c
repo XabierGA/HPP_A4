c65aa07eb123c1cb972434e74b421b03adc6b745
Bernat Gilboy Sopena <>
2019-02-18 19:46:35 +0100

    started divide and conquer algorism, created node structure and working on insert function

--- divide_conquer.c
+++ divide_conquer.c
@@ -51,8 +51,11 @@ int int main(int argc, char const *args[]) {
   const float epsilon_0 = 0.001;
 
   //look up table for powers of two
-  
-
+  const K=200;
+  const double pow_2[K];
+  pow[0]=1.0;
+  for(int i=1; i< K; i++)
+    pow_2[i]= pow_2[i-1]/2;
 
   //declare node structure
   //maybe good idea is to compact it! (ie no padding)
@@ -60,7 +63,8 @@ int int main(int argc, char const *args[]) {
     double x_lim; //x division
     double y_lim; // y division
     double width; //width of the box
-    double cm; //center of mass
+    double cm_x;
+    double cm_y; //center of mass
     double tot_mass; //total mass
     struct tree_node *left;
     struct tree_node *right;
@@ -75,13 +79,32 @@ int int main(int argc, char const *args[]) {
     //insert node in tree with given coordinates x and y.
     //edits center of mass at each level.
     int up, right, depth;
-    double parent_x_lim, parent_y_lim;
+    double x_lim_parent, y_lim_parent;
 
     //If node is null create it!
     if((*node) == NULL){
       //allocate mem for new node
       *node = (node_t*)malloc(sizeof(node_t));
-      (*node)->x_lim =
+      //if right is true, we go 2^(depth) to the right
+      (*node)->x_lim = x_lim_parent + right*pow[depth-1] - pow[depth];
+      (*node)->y_lim =  y_lim_parent + up*pow_2[depth-1] - pow[depth];
+      (*node)-> width = pow_2[depth];
+      (*node)-> cm_x = x_pos;
+      (*node)-> cm_y = y_pos;
+      (*node)-> total_mass = mass;
+      (*node)->left = NULL;
+      (*node)->right = NULL;
+      (*node)->down = NULL;
+      (*node)->up = NULL;
+    }else{
+      //update center of mass new_cm = old_mass*old_cm + pos_new_particle*mass_new_particle
+      (*node)->cm_x += pos_x*(mass/total_mass); // Note: This maybe we can do more efficently
+      (*node)->cm_y += pos_y*(mass/total_mass);
+      (*node)->total_mass+=mass;
+      if( x_pos > (*node)->x_lim){
+        //go to the right
+        
+      }
     }
 
   }
